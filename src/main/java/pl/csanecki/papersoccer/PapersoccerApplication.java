88b8b288f513cc3eca6a0030f554d0f722bf4f76
Czarek <csanecki@gmail.com>
2019-11-25 22:38:16 +0100

    initial commit

---
+++ src/main/java/pl/csanecki/papersoccer/PapersoccerApplication.java
@@ -0,0 +1,13 @@
+package pl.csanecki.papersoccer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapersoccerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PapersoccerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/csanecki/papersoccer/PapersoccerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.csanecki.papersoccer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PapersoccerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
