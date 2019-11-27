a247c9562aa2bffe675ab1c1ae60148d69ca4c3e
Cezary Sanecki <csanecki@gmail.com>
2019-11-27 09:35:01 +0100

    some test cases about moving ball

--- src/main/java/pl/csanecki/papersoccer/logic/Board.java
+++ src/main/java/pl/csanecki/papersoccer/logic/Board.java
@@ -1,11 +1,68 @@
 package pl.csanecki.papersoccer.logic;
 
 public class Board {
-    final int boardWidth;
-    final int boardHeight;
+    private final int boardWidth;
+    private final int boardHeight;
+
+    private int ballX;
+    private int ballY;
 
     public Board(int halfWidth, int halfHeight) {
         this.boardWidth = halfWidth * 2 + 1;
         this.boardHeight = halfHeight * 2 + 1;
+
+        this.ballX = halfWidth + 1;
+        this.ballY = halfHeight + 1;
+    }
+
+    public int getBoardWidth() {
+        return boardWidth;
+    }
+
+    public int getBoardHeight() {
+        return boardHeight;
+    }
+
+    public int getBallX() {
+        return ballX;
+    }
+
+    public int getBallY() {
+        return ballY;
+    }
+
+    public void moveBall(int direction) {
+        switch (direction) {
+            case 8:
+                ballY++;
+                break;
+            case 9:
+                ballY++;
+                ballX++;
+                break;
+            case 6:
+                ballX++;
+                break;
+            case 3:
+                ballY--;
+                ballX++;
+                break;
+            case 2:
+                ballY--;
+                break;
+            case 1:
+                ballY--;
+                ballX--;
+                break;
+            case 4:
+                ballX--;
+                break;
+            case 7:
+                ballY++;
+                ballX--;
+                break;
+            default:
+                throw new RuntimeException("Niedozwolone przemieszczenie piłki");
+        }
     }
 }
--- src/test/java/pl/csanecki/papersoccer/logic/BoardTest.java
+++ src/test/java/pl/csanecki/papersoccer/logic/BoardTest.java
@@ -1,17 +1,54 @@
 package pl.csanecki.papersoccer.logic;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class BoardTest {
+    private int halfWidth = 4;
+    private int halfHeight = 5;
+
+    private Board board;
+
+    @BeforeEach
+    void initializeBoard() {
+        board = new Board(halfWidth, halfHeight);
+    }
+
     @Test
-    void createBoardWhichWidthAndHeightIsTwiceBiggerPlusOneAsPassed() {
-        int halfWidth = 5;
-        int halfHeight = 10;
+    void givenHalfWidthThenBoardWidthIsTwiceWiderPlusOne() {
+        Assertions.assertEquals(board.getBoardWidth(), halfWidth * 2 + 1, "Board should be twice wider plus one that it was passed");
+    }
 
-        Board board = new Board(halfWidth, halfHeight);
+    @Test
+    void givenHalfHeightThenBoardHeightIsTwiceHigherPlusOne() {
+        Assertions.assertEquals(board.getBoardHeight(), halfHeight * 2 + 1, "Board should be twice higher plus one that it was passed");
+    }
 
-        Assertions.assertEquals(board.boardWidth, halfWidth * 2 + 1, "Board should be twice wider plus one that it was passed");
-        Assertions.assertEquals(board.boardHeight, halfHeight * 2 + 1, "Board should be twice higher plus one that it was passed");
+    @Test
+    void givenHalfWidthAndHalfHeightThenBallIsInTheMiddleOfBoard() {
+        Assertions.assertEquals(board.getBallX(), halfWidth + 1, "Ball should be in middle of board's width");
+        Assertions.assertEquals(board.getBallY(), halfHeight + 1, "Ball should be in middle of board's height");
+    }
+
+    @Test
+    void givenMoveBallToSouthThenBallIsOneDownFromMiddleOfBoard() {
+        board.moveBall(2);
+
+        Assertions.assertEquals(board.getBallY(), halfHeight, "Ball should be one down from middle of board");
+    }
+
+    @Test
+    void givenMoveBallToEastThenBallIsOneRightFromMiddleOfBoard() {
+        board.moveBall(6);
+
+        Assertions.assertEquals(board.getBallX(), halfWidth + 2, "Ball should be one right from middle of board");
+    }
+
+    @Test
+    void givenWrongDirectionOfBallThenRuntimeException() {
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            board.moveBall(5);
+        });
     }
 }
