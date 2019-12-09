b99f534da2874a217677c4dcc302df9936422a30
Cezary Sanecki <csanecki@gmail.com>
2019-12-09 09:27:04 +0100

    refactored conception of width and height

--- src/main/java/pl/csanecki/papersoccer/logic/Board.java
+++ src/main/java/pl/csanecki/papersoccer/logic/Board.java
@@ -1,117 +1,18 @@
 package pl.csanecki.papersoccer.logic;
 
 public class Board {
-    private final int boardWidth;
-    private final int boardHeight;
+    private final static int MIN_DIMENSION = 4;
 
-    private int ballX;
-    private int ballY;
-
-    public Board(int halfWidth, int halfHeight) {
-        checkPassedDimension(halfWidth);
-        checkPassedDimension(halfHeight);
-
-        this.boardWidth = halfWidth * 2 + 1;
-        this.boardHeight = halfHeight * 2 + 1;
-
-        this.ballX = halfWidth;
-        this.ballY = halfHeight;
+    public Board(int width, int height) {
+        checkDimension(width);
+        checkDimension(height);
     }
 
-    private void checkPassedDimension(int dimension) {
-        if(dimension < 2) {
-            throw new RuntimeException("Passed dimension is too small");
+    private void checkDimension(int dimension) {
+        if(dimension < MIN_DIMENSION) {
+            throw new RuntimeException("Dimension is to small");
+        } else if(dimension % 2 == 1) {
+            throw new RuntimeException("Dimension cannot be odd");
         }
-    }
-
-    public void moveBall(int direction) {
-        switch (direction) {
-            case 8:
-                moveNorth();
-                break;
-            case 2:
-                moveSouth();
-                break;
-            case 4:
-                moveWest();
-                break;
-            case 6:
-                moveEast();
-                break;
-            case 7:
-                moveNorth();
-                moveWest();
-                break;
-            case 9:
-                moveNorth();
-                moveEast();
-                break;
-            case 3:
-                moveSouth();
-                moveEast();
-                break;
-            case 1:
-                moveSouth();
-                moveWest();
-                break;
-            default:
-                throw new RuntimeException("Not supported operation on ball");
-        }
-    }
-
-    private void moveNorth() {
-        if(checkYAxisMovementValidation()) {
-            ballY--;
-        }
-    }
-
-    private void moveSouth() {
-        if(checkYAxisMovementValidation()) {
-            ballY++;
-        }
-    }
-
-    private void moveWest() {
-        if(checkXAxisMovementValidation()) {
-            ballX--;
-        }
-    }
-
-    private void moveEast() {
-        if(checkXAxisMovementValidation()) {
-            ballX++;
-        }
-    }
-
-    private boolean checkYAxisMovementValidation() {
-        if(ballY - 1 >= 0 && ballY + 1 < boardHeight) {
-            return true;
-        }
-
-        throw new RuntimeException("Ball cannot be outside of board");
-    }
-
-    private boolean checkXAxisMovementValidation() {
-        if(ballX - 1 >= 0 && ballX + 1 < boardWidth) {
-            return true;
-        }
-
-        throw new RuntimeException("Ball cannot be outside of board");
-    }
-
-    public int getBoardWidth() {
-        return boardWidth;
-    }
-
-    public int getBoardHeight() {
-        return boardHeight;
-    }
-
-    public int getBallX() {
-        return ballX;
-    }
-
-    public int getBallY() {
-        return ballY;
     }
 }
--- src/test/java/pl/csanecki/papersoccer/logic/BoardTest.java
+++ src/test/java/pl/csanecki/papersoccer/logic/BoardTest.java
@@ -1,170 +1,29 @@
 package pl.csanecki.papersoccer.logic;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInfo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class BoardTest {
-    private int halfWidth = 4;
-    private int halfHeight = 5;
+    private int width = 8;
+    private int height = 10;
 
-    private Board board;
+    @Test
+    void givenWidthTooSmallThenRuntimeException() {
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> {
+            Board board = new Board(2, height);
+        }, "The width is to small");
 
-    @BeforeEach
-    void initializeBoard(TestInfo testInfo) {
-        if(!testInfo.getTags().contains("SkipSetup")) {
-            board = new Board(halfWidth, halfHeight);
-        }
+        assertEquals("Dimension is to small", exception.getMessage());
     }
 
     @Test
-    @Tag("SkipSetup")
-    void givenHalfWidthWhenBoardWidthTooSmallThenRuntimeException() {
-        assertThrows(RuntimeException.class, () -> {
-            board = new Board(1, halfHeight);
-        });
-    }
+    void givenOddHeightThenRuntimeException() {
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> {
+            Board board = new Board(width, 7);
+        }, "The height cannot be odd");
 
-    @Test
-    @Tag("SkipSetup")
-    void givenHalfHeightWhenBoardHeightTooSmallThenRuntimeException() {
-        assertThrows(RuntimeException.class, () -> {
-            board = new Board(halfWidth, 1);
-        });
-    }
-
-    @Test
-    void givenHalfWidthThenBoardWidthIsTwiceWiderPlusOne() {
-        assertEquals(halfWidth * 2 + 1, board.getBoardWidth(), "Board should be twice wider plus one that it was passed");
-    }
-
-    @Test
-    void givenHalfHeightThenBoardHeightIsTwiceHigherPlusOne() {
-        assertEquals(halfHeight * 2 + 1, board.getBoardHeight(), "Board should be twice higher plus one that it was passed");
-    }
-
-    @Test
-    void givenHalfWidthAndHalfHeightThenBallIsInTheMiddleOfBoard() {
-        assertEquals(halfWidth, board.getBallX(), "Ball should be in middle of board's width");
-        assertEquals(halfHeight, board.getBallY(), "Ball should be in middle of board's height");
-    }
-
-    @Test
-    void given8WhenMoveBallThenBallMovesToNorth() {
-        board.moveBall(8);
-
-        assertEquals(halfHeight - 1, board.getBallY(), "Ball should be one top from middle of board");
-    }
-
-    @Test
-    void given2WhenMoveBallThenBallMovesToSouth() {
-        board.moveBall(2);
-
-        assertEquals(halfHeight + 1, board.getBallY(), "Ball should be one bottom from middle of board");
-    }
-
-    @Test
-    void given4WhenMoveBallThenBallMovesToWest() {
-        board.moveBall(4);
-
-        assertEquals(halfWidth - 1, board.getBallX(), "Ball should be one left from middle of board");
-    }
-
-    @Test
-    void given6WhenMoveBallThenBallMovesToEast() {
-        board.moveBall(6);
-
-        assertEquals(halfWidth + 1, board.getBallX(), "Ball should be one right from middle of board");
-    }
-
-    @Test
-    void given7WhenMoveBallThenBallMovesToNorthEast() {
-        board.moveBall(7);
-
-        assertEquals(halfWidth - 1, board.getBallX(), "Ball should be one left from middle of board");
-        assertEquals(halfHeight - 1, board.getBallY(), "Ball should be one top from middle of board");
-    }
-
-    @Test
-    void given9WhenMoveBallThenBallMovesToNorthWest() {
-        board.moveBall(9);
-
-        assertEquals(halfWidth + 1, board.getBallX(), "Ball should be one right from middle of board");
-        assertEquals(halfHeight - 1, board.getBallY(), "Ball should be one top from middle of board");
-    }
-
-    @Test
-    void given3WhenMoveBallThenBallMovesToSouthWest() {
-        board.moveBall(3);
-
-        assertEquals(halfWidth + 1, board.getBallX(), "Ball should be one right from middle of board");
-        assertEquals(halfHeight + 1, board.getBallY(), "Ball should be one bottom from middle of board");
-    }
-
-    @Test
-    void given1WhenMoveBallThenBallMovesToSouthEast() {
-        board.moveBall(1);
-
-        assertEquals(halfWidth - 1, board.getBallX(), "Ball should be one left from middle of board");
-        assertEquals(halfHeight + 1, board.getBallY(), "Ball should be one bottom from middle of board");
-    }
-
-    @Test
-    void givenNotSupportNumberWhenMoveBallThenRuntimeException() {
-        assertThrows(RuntimeException.class, () -> {
-            board.moveBall(5);
-        });
-    }
-
-    @Test
-    void givenMovingEastWhenMoveOutOfBoardThenRuntimeException() {
-        for (int i = 0; i < halfWidth; i++) {
-            board.moveBall(6);
-        }
-
-        assertThrows(RuntimeException.class, () -> {
-            board.moveBall(6);
-        });
-    }
-
-    @Test
-    void givenMovingWestWhenMoveOutOfBoardThenRuntimeException() {
-        for (int i = 0; i < halfWidth; i++) {
-            board.moveBall(4);
-        }
-
-        assertThrows(RuntimeException.class, () -> {
-            board.moveBall(4);
-        });
-    }
-
-    @Test
-    void givenMovingOnWestAndFullNorthWhenMoveOutOfBoardThenRuntimeException() {
-        board.moveBall(4);
-
-        for (int i = 0; i < halfHeight; i++) {
-            board.moveBall(8);
-        }
-
-        assertThrows(RuntimeException.class, () -> {
-            board.moveBall(8);
-        });
-    }
-
-    @Test
-    void givenMovingOnWestAndFullSouthWhenMoveOutOfBoardThenRuntimeException() {
-        board.moveBall(4);
-
-        for (int i = 0; i < halfHeight; i++) {
-            board.moveBall(2);
-        }
-
-        assertThrows(RuntimeException.class, () -> {
-            board.moveBall(2);
-        });
+        assertEquals("Dimension cannot be odd", exception.getMessage());
     }
 }
