1e6aa46ab63b371dfd48ca6967496fff45c300fe
Cezary Sanecki <csanecki@gmail.com>
2019-12-09 11:30:11 +0100

    added new basic tests to make board and move ball

--- src/main/java/pl/csanecki/papersoccer/logic/Board.java
+++ src/main/java/pl/csanecki/papersoccer/logic/Board.java
@@ -3,9 +3,21 @@ package pl.csanecki.papersoccer.logic;
 public class Board {
     private final static int MIN_DIMENSION = 4;
 
+    private final int width;
+    private final int height;
+
+    private int ballX;
+    private int ballY;
+
     public Board(int width, int height) {
         checkDimension(width);
         checkDimension(height);
+
+        this.width = width;
+        this.height = height;
+
+        this.ballX = width / 2;
+        this.ballY = height / 2;
     }
 
     private void checkDimension(int dimension) {
@@ -13,6 +25,55 @@ public class Board {
             throw new RuntimeException("Dimension is to small");
         } else if(dimension % 2 == 1) {
             throw new RuntimeException("Dimension cannot be odd");
+        }
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getBallX() {
+        return ballX;
+    }
+
+    public int getBallY() {
+        return ballY;
+    }
+
+    public void play(int move) {
+        switch (move) {
+            case 1:
+                ballY++;
+                ballX--;
+                break;
+            case 2:
+                ballY++;
+                break;
+            case 3:
+                ballY++;
+                ballX++;
+                break;
+            case 4:
+                ballX--;
+                break;
+            case 6:
+                ballX++;
+                break;
+            case 7:
+                ballY--;
+                ballX--;
+                break;
+            case 8:
+                ballY--;
+                break;
+            case 9:
+                ballY--;
+                ballX++;
+                break;
         }
     }
 }
--- src/test/java/pl/csanecki/papersoccer/logic/BoardTest.java
+++ src/test/java/pl/csanecki/papersoccer/logic/BoardTest.java
@@ -1,5 +1,6 @@
 package pl.csanecki.papersoccer.logic;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -9,10 +10,17 @@ public class BoardTest {
     private int width = 8;
     private int height = 10;
 
+    private Board board;
+
+    @BeforeEach
+    void setUp() {
+        board = new Board(width, height);
+    }
+
     @Test
     void givenWidthTooSmallThenRuntimeException() {
         RuntimeException exception = assertThrows(RuntimeException.class, () -> {
-            Board board = new Board(2, height);
+            new Board(2, height);
         }, "The width is to small");
 
         assertEquals("Dimension is to small", exception.getMessage());
@@ -21,9 +29,81 @@ public class BoardTest {
     @Test
     void givenOddHeightThenRuntimeException() {
         RuntimeException exception = assertThrows(RuntimeException.class, () -> {
-            Board board = new Board(width, 7);
+            new Board(width, 7);
         }, "The height cannot be odd");
 
         assertEquals("Dimension cannot be odd", exception.getMessage());
+    }
+
+    @Test
+    void givenProperWidthThenBoardWidthIsSet() {
+        assertEquals(width, board.getWidth());
+    }
+
+    @Test
+    void givenProperHeightThenBoardHeightIsSet() {
+        assertEquals(height, board.getHeight());
+    }
+
+    @Test
+    void givenWidthAndHeightWhenBoardIsInitiatedThenBallIsCenteredHorizontally() {
+        assertEquals(width / 2, board.getBallX());
+    }
+
+    @Test
+    void givenWidthAndHeightWhenBoardIsInitiatedThenBallIsCenteredVertically() {
+        assertEquals(height / 2, board.getBallY());
+    }
+
+    @Test
+    void given8ThenBallMoveNorth() {
+        board.play(8);
+        assertEquals((height / 2) - 1, board.getBallY());
+    }
+
+    @Test
+    void given2ThenBallMoveSouth() {
+        board.play(2);
+        assertEquals((height / 2) + 1, board.getBallY());
+    }
+
+    @Test
+    void given4ThenBallMoveWest() {
+        board.play(4);
+        assertEquals((width / 2) - 1, board.getBallX());
+    }
+
+    @Test
+    void given6ThenBallMoveEast() {
+        board.play(6);
+        assertEquals((width / 2) + 1, board.getBallX());
+    }
+
+    @Test
+    void given7ThenBallMoveNorthWest() {
+        board.play(7);
+        assertEquals((height / 2) - 1, board.getBallY());
+        assertEquals((width / 2) - 1, board.getBallX());
+    }
+
+    @Test
+    void given9ThenBallMoveNorthEast() {
+        board.play(9);
+        assertEquals((height / 2) - 1, board.getBallY());
+        assertEquals((width / 2) + 1, board.getBallX());
+    }
+
+    @Test
+    void given1ThenBallMoveSouthWest() {
+        board.play(1);
+        assertEquals((height / 2) + 1, board.getBallY());
+        assertEquals((width / 2) - 1, board.getBallX());
+    }
+
+    @Test
+    void given3ThenBallMoveSouthEast() {
+        board.play(3);
+        assertEquals((height / 2) + 1, board.getBallY());
+        assertEquals((width / 2) + 1, board.getBallX());
     }
 }
