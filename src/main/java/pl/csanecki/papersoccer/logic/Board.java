65df1cf2f44c9d6d3e01c829ca1312f20a319ffe
csanecki@gmail.com <csanecki@gmail.com>
2019-12-11 13:27:37 +0100

    tested movement of ball in Board class

--- src/main/java/pl/csanecki/papersoccer/logic/Board.java
+++ src/main/java/pl/csanecki/papersoccer/logic/Board.java
@@ -2,6 +2,7 @@ package pl.csanecki.papersoccer.logic;
 
 public class Board {
     private Node[][] nodesNetwork;
+    private Node ballNode;
 
     public Board(int width, int height) {
         nodesNetwork = new Node[width + 1][height + 1];
@@ -14,6 +15,45 @@ public class Board {
                 nodesNetwork[x][y] = new Node(x, y);
             }
         }
+
+        setBallInTheMiddle();
+    }
+
+    private void setBallInTheMiddle() {
+        int middleOfWidth = (nodesNetwork.length - 1) / 2;
+        int middleOfHeight = (nodesNetwork[0].length - 1) / 2;
+
+        ballNode = nodesNetwork[middleOfWidth][middleOfHeight];
+        ballNode.setContainsBall(true);
+    }
+
+    public void moveBall(int move) {
+        ballNode.setContainsBall(false);
+        ballNode = getNodeAfterMove(move);
+        ballNode.setContainsBall(true);
+    }
+
+    private Node getNodeAfterMove(int move) {
+        switch (move) {
+            case 1:
+                return nodesNetwork[ballNode.getX() - 1][ballNode.getY() + 1];
+            case 2:
+                return nodesNetwork[ballNode.getX()][ballNode.getY() + 1];
+            case 3:
+                return nodesNetwork[ballNode.getX() + 1][ballNode.getY() + 1];
+            case 4:
+                return nodesNetwork[ballNode.getX() - 1][ballNode.getY()];
+            case 6:
+                return nodesNetwork[ballNode.getX() + 1][ballNode.getY()];
+            case 7:
+                return nodesNetwork[ballNode.getX() - 1][ballNode.getY() - 1];
+            case 8:
+                return nodesNetwork[ballNode.getX()][ballNode.getY() - 1];
+            case 9:
+                return nodesNetwork[ballNode.getX() + 1][ballNode.getY() - 1];
+        }
+
+        throw new RuntimeException("Not allowed movement of ball");
     }
 
     public Node[][] getNodesNetwork() {
--- src/test/java/pl/csanecki/papersoccer/logic/BoardTest.java
+++ src/test/java/pl/csanecki/papersoccer/logic/BoardTest.java
@@ -3,7 +3,7 @@ package pl.csanecki.papersoccer.logic;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.springframework.test.util.AssertionErrors.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BoardTest {
     private int width = 8;
@@ -22,6 +22,104 @@ public class BoardTest {
 
         Node expected = new Node(3, 4);
 
-        assertEquals("Nodes network is wrongly setted", nodesNetwork[3][4], expected);
+        assertEquals(nodesNetwork[3][4], expected, "Nodes network is wrongly set");
+    }
+
+    @Test
+    void givenBoardInitiatedWhenWidthAndHeightIsSetThenBallIsInTheMiddleOfBoard() {
+        Node[][] nodesNetwork = board.getNodesNetwork();
+
+        Node nodeWithBall = nodesNetwork[width / 2][height / 2];
+
+        assertTrue(nodeWithBall.containsBall(), "Middle node does not contain ball");
+    }
+
+    @Test
+    void whenMoveBallLeftThenNodeOnTheLeftFromMiddleHasBall() {
+        board.moveBall(4);
+
+        Node[][] nodesNetwork = board.getNodesNetwork();
+        Node nodeWithBall = nodesNetwork[(width / 2) - 1][height / 2];
+
+        assertTrue(nodeWithBall.containsBall(), "Node does not contain ball");
+    }
+
+    @Test
+    void whenMoveBallRightThenNodeOnTheRightFromMiddleHasBall() {
+        board.moveBall(6);
+
+        Node[][] nodesNetwork = board.getNodesNetwork();
+        Node nodeWithBall = nodesNetwork[(width / 2) + 1][height / 2];
+
+        assertTrue(nodeWithBall.containsBall(), "Node does not contain ball");
+    }
+
+    @Test
+    void whenMoveBallTopThenNodeOnTheTopFromMiddleHasBall() {
+        board.moveBall(8);
+
+        Node[][] nodesNetwork = board.getNodesNetwork();
+        Node nodeWithBall = nodesNetwork[width / 2][(height / 2) - 1];
+
+        assertTrue(nodeWithBall.containsBall(), "Node does not contain ball");
+    }
+
+    @Test
+    void whenMoveBallDownThenNodeOnTheDownFromMiddleHasBall() {
+        board.moveBall(2);
+
+        Node[][] nodesNetwork = board.getNodesNetwork();
+        Node nodeWithBall = nodesNetwork[width / 2][(height / 2) + 1];
+
+        assertTrue(nodeWithBall.containsBall(), "Node does not contain ball");
+    }
+
+    @Test
+    void whenNotAllowedMovementThenRuntimeException() {
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> {
+            board.moveBall(5);
+        }, "The movement is incorrectly allowed");
+
+        assertEquals("Not allowed movement of ball", exception.getMessage());
+    }
+
+    @Test
+    void whenMoveBallTopLeftThenNodeOnTheTopLeftFromMiddleHasBall() {
+        board.moveBall(7);
+
+        Node[][] nodesNetwork = board.getNodesNetwork();
+        Node nodeWithBall = nodesNetwork[(width / 2) - 1][(height / 2) - 1];
+
+        assertTrue(nodeWithBall.containsBall(), "Node does not contain ball");
+    }
+
+    @Test
+    void whenMoveBallTopRightThenNodeOnTheTopRightFromMiddleHasBall() {
+        board.moveBall(9);
+
+        Node[][] nodesNetwork = board.getNodesNetwork();
+        Node nodeWithBall = nodesNetwork[(width / 2) + 1][(height / 2) - 1];
+
+        assertTrue(nodeWithBall.containsBall(), "Node does not contain ball");
+    }
+
+    @Test
+    void whenMoveBallDownLeftThenNodeOnTheDownLeftFromMiddleHasBall() {
+        board.moveBall(1);
+
+        Node[][] nodesNetwork = board.getNodesNetwork();
+        Node nodeWithBall = nodesNetwork[(width / 2) - 1][(height / 2) + 1];
+
+        assertTrue(nodeWithBall.containsBall(), "Node does not contain ball");
+    }
+
+    @Test
+    void whenMoveBallDownRightThenNodeOnTheDownRightFromMiddleHasBall() {
+        board.moveBall(3);
+
+        Node[][] nodesNetwork = board.getNodesNetwork();
+        Node nodeWithBall = nodesNetwork[(width / 2) + 1][(height / 2) + 1];
+
+        assertTrue(nodeWithBall.containsBall(), "Node does not contain ball");
     }
 }
