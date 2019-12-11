b482e38ae30dccb1191c8758f95b1673db31247f
csanecki@gmail.com <csanecki@gmail.com>
2019-12-11 15:35:24 +0100

    tested winning possibilities for Player2

--- src/main/java/pl/csanecki/papersoccer/logic/Board.java
+++ src/main/java/pl/csanecki/papersoccer/logic/Board.java
@@ -27,10 +27,24 @@ public class Board {
         ballNode.setContainsBall(true);
     }
 
-    public void moveBall(int move) {
+    public String moveBall(int move) {
+        if(ballNode.getX() == getBoardWidth() / 2 && ballNode.getY() == 0 && move == 8) {
+            return "Player2 Wins";
+        }
+
+        if(ballNode.getX() == getBoardWidth() / 2 - 1 && ballNode.getY() == 0 && move == 9) {
+            return "Player2 Wins";
+        }
+
+        if(ballNode.getX() == getBoardWidth() / 2 + 1 && ballNode.getY() == 0 && move == 7) {
+            return "Player2 Wins";
+        }
+
         ballNode.setContainsBall(false);
         ballNode = getNodeAfterMove(move);
         ballNode.setContainsBall(true);
+
+        return "Game Underway";
     }
 
     private Node getNodeAfterMove(int move) {
@@ -42,8 +56,16 @@ public class Board {
             case 3:
                 return nodesNetwork[ballNode.getX() + 1][ballNode.getY() + 1];
             case 4:
+                if(ballNode.getX() - 1 < 0) {
+                    throw new RuntimeException("Not allowed to move out of board");
+                }
+
                 return nodesNetwork[ballNode.getX() - 1][ballNode.getY()];
             case 6:
+                if(ballNode.getX() + 1 > getBoardWidth()) {
+                    throw new RuntimeException("Not allowed to move out of board");
+                }
+
                 return nodesNetwork[ballNode.getX() + 1][ballNode.getY()];
             case 7:
                 return nodesNetwork[ballNode.getX() - 1][ballNode.getY() - 1];
@@ -54,6 +76,10 @@ public class Board {
         }
 
         throw new RuntimeException("Not allowed movement of ball");
+    }
+
+    private int getBoardWidth() {
+        return nodesNetwork.length - 1;
     }
 
     public Node[][] getNodesNetwork() {
--- src/test/java/pl/csanecki/papersoccer/logic/BoardTest.java
+++ src/test/java/pl/csanecki/papersoccer/logic/BoardTest.java
@@ -122,4 +122,66 @@ public class BoardTest {
 
         assertTrue(nodeWithBall.containsBall(), "Node does not contain ball");
     }
+
+    @Test
+    void givenMoveBallLeftToBoardWhenMoveBallLeftThenRuntimeException() {
+        for (int i = 0; i < width / 2; i++) {
+            board.moveBall(4);
+        }
+
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> {
+            board.moveBall(4);
+        }, "The movement is incorrectly allowed");
+
+        assertEquals("Not allowed to move out of board", exception.getMessage());
+    }
+
+    @Test
+    void givenMoveBallRightToBoardWhenMoveBallRightThenRuntimeException() {
+        for (int i = 0; i < width / 2; i++) {
+            board.moveBall(6);
+        }
+
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> {
+            board.moveBall(6);
+        }, "The movement is incorrectly allowed");
+
+        assertEquals("Not allowed to move out of board", exception.getMessage());
+    }
+
+    @Test
+    void givenMoveBallTopToBoardEdgeWhenMoveBallTopThenWinner() {
+        for (int i = 0; i < height / 2; i++) {
+            board.moveBall(8);
+        }
+
+        assertEquals("Player2 Wins", board.moveBall(8));
+    }
+
+    @Test
+    void givenMoveBallTopToBoardAndOneLeftWhenMoveBallTopRightThenWinner() {
+        for (int i = 0; i < height / 2; i++) {
+            board.moveBall(8);
+        }
+
+        board.moveBall(4);
+
+        assertEquals("Player2 Wins", board.moveBall(9));
+    }
+
+    @Test
+    void givenMoveBallTopToBoardAndOneRightWhenMoveBallTopLeftThenWinner() {
+        for (int i = 0; i < height / 2; i++) {
+            board.moveBall(8);
+        }
+
+        board.moveBall(6);
+
+        assertEquals("Player2 Wins", board.moveBall(7));
+    }
+
+    @Test
+    void whenMoveBallAndNoWinnerThenGameUnderway() {
+        assertEquals("Game Underway", board.moveBall(4));
+    }
 }
